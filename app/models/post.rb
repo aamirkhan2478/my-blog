1561f2490dead9019f305a6024da5bf0aa551085
aamir <aamir.khan2478@gmail.com>
2022-11-23 12:49:11 +0500

    Fixed linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
 
-  belongs_to :author, class_name: "User", foreign_key: "author_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def update_post_counter
     author.increment!(:posts_counter)
