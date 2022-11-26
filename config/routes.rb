5740304b4f40ada9d0d84af0286b12be6871aaa9
aamir <aamir.khan2478@gmail.com>
2022-11-26 23:52:03 +0500

    Add all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: "users#index"
+  get "users/:author_id/posts", to: "posts#index", as: "user_posts"
+  get "users/:author_id/posts/:id", to: "posts#show", as: "user_post"
+  get "users/:id", to: "users#show", as: "user"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "posts/show.html.erb", type: :view do
+  it "renders 'show' template" do
+    render :template => "posts/show"
+    expect(rendered).to match /Here show post by ID/
+  end
+end
