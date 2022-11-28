ed476f1b7280b37ea8227ddcf64d8bffa741b5a1
aamir <aamir.khan2478@gmail.com>
2022-11-28 11:22:50 +0500

    Create all required views for my blog app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   def index
+    @user = User.find(params[:author_id])
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:author_id])
+    @comments = @post.comments
   end
 end
