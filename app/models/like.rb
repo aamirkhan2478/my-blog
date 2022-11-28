556fea6e26debfb3e3f0f4da2b065fb7f70efb3b
aamir <aamir.khan2478@gmail.com>
2022-11-28 12:10:41 +0500

    Fixed linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,7 +4,7 @@ class Like < ApplicationRecord
   after_save :update_like_counter
 
   private
-  
+
   def update_like_counter
     post.increment!(:likes_counter)
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,19 +1,19 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success and check correct placeholder text." do
-      get "/"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    it 'returns http success and check correct placeholder text.' do
+      get '/'
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include("Here is a list of users") 
+      expect(response.body).to include('Here is a list of users')
     end
   end
 
-  describe "GET /show" do
-    it "returns http success and check correct placeholder text." do
-      get "/users/:id"
+  describe 'GET /show' do
+    it 'returns http success and check correct placeholder text.' do
+      get '/users/:id'
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include("Here show user by id")
+      expect(response.body).to include('Here show user by id')
     end
   end
 end
