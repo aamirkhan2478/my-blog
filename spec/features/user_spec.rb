a3688a25590a0eac0c3c0feadb3224c9b5326715
aamir <aamir.khan2478@gmail.com>
2022-12-05 23:05:07 +0500

    Fix required changes

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -22,9 +22,11 @@ RSpec.describe User, type: :system do
     end
 
     it "When I click on a user, I am redirected to that user's show page." do
-      visit root_path(subject)
+      User.delete_all
+      user = User.create(name: 'John', posts_counter: 30, photo: 'https://randomuser.me/api/portraits/men/70.jpg', bio: 'Teacher from Poland.') 
+      visit root_path(user)
       click_on 'John'
-      visit user_path(subject.id)
+      visit user_path(user.id) 
       page.has_content?('John')
     end
   end
