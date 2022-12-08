e823d7cc64bff843dc4abc650f42f30fa245e1bd
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-12-08 08:51:36 +0100

    Add cancancan to post delete button

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new
-    if user.role == "admin"
+    if user.role == 'admin'
       can :manage, :all
     else
       can :destroy, Post do |post|
